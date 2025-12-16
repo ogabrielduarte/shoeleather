e2fb9697493ee5418883e54cb1c6692b2823130e
ogabrielduarte <gsdo1@aluno.ifal.edu.br>
2025-12-15 21:28:53 -0300

    Initialize Express app with routes and database sync

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const routes = require('./routes/routes');
+const { sequelize } = require('./models/models');
+
+const app = express();
+app.use(express.json());
+
+// rotas
+app.use('/api', routes);
+
+// conexão banco
+sequelize.sync()
+  .then(() => console.log('Banco sincronizado'))
+  .catch(err => console.error(err));
+
+module.exports = app;
